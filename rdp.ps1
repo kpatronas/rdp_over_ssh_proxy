234e5ac9f60ae6410b8c3ee48081bd917fa30dc2
Konstantinos Patronas <kpatronas@gmail.com>
2021-09-15 15:52:17 +0300

    Add files via upload

---
+++ rdp.ps1
@@ -0,0 +1,36 @@
+param([Parameter(Mandatory=$True)]$server,
+      [Parameter(Mandatory=$True)]$gw,
+      [Parameter(Mandatory=$True)]$user,
+      [Parameter(Mandatory=$True)]$rdp_user,
+      [Parameter(Mandatory=$False)]$rdp_pass)
+
+while($True)
+{
+ $lport     = (Get-Random -Minimum 4097 -Maximum 65535)
+ $port_used = (Get-NetTCPConnection | where Localport -eq $lport)
+ if(!$port_used)
+ {
+   break
+ }
+}
+
+$tunnel       = Start-Process ssh   -ArgumentList "-N -l $user $gw -L ${lport}:${server}:3389" -Verb open -PassThru -windowstyle hidden
+$tunnel_is_up = Get-process | where Id -eq $tunnel.Id
+
+if($tunnel_is_up)
+{ 
+ cmdkey /generic:localhost /user:${rdp_user} /pass:${rdp_pass} > $null
+ $rdp    = Start-Process mstsc -ArgumentList "/f /V:localhost:${lport}" -PassThru
+}
+
+while($True)
+{
+ Sleep 5
+ $is_running = Get-process | where Id -eq $rdp.Id
+ if(!$is_running)
+ {
+   $tunnel | Stop-Process -Force
+   cmdkey /delete:localhost > $null
+   break
+ }
+}
